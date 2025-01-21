fab92d2daba4d6dd6b6ada74884c64c1999c37bb
Emma Luk <emma07315@gmail.com>
2025-01-21 16:43:34 -0700

    client and server connections verified
    need to implement headers

--- Project1/PA1-A/HTTPproxy.py
+++ Project1/PA1-A/HTTPproxy.py
@@ -9,16 +9,26 @@ def ctrl_c_pressed(signal, frame):
 	sys.exit(0)
 
 # TODO: Put function definitions here
-def parse_http_request(request_line: str):
+def parse_http_request(request: str):
     try:
-        # Split the request line into components
-        method, url, http_version = request_line.split()
+        method, url, http_version = request.split()
 
-        if method.upper() == 'GET':
+        if method.upper() == "GET":
+            # # TODO: Parse headers
+            # headers = {}
+            # for line in lines[1:]:
+            #     if not line.strip():  # Stop at the first empty line (end of headers)
+            #         break
+            #     if ": " in line:
+            #         header_name, header_value = line.split(": ", 1)
+            #         headers[header_name] = header_value
+            #     else:
+            #         raise ValueError("400 Bad Request: Malformed header.")
             return {
                 "method": method,
                 "url": url,
-                "http_version": http_version
+                "http_version": http_version,
+                # "headers": headers
             }
         else: 
             raise ValueError("501 Not Implemented")    
@@ -37,27 +47,29 @@ def parse_url(url):
     else:
         hostname = url
         path = "/"
-
-    # Handle optional port in URL
     if ":" in hostname:
         hostname, port = hostname.split(":")
         port = int(port)
     else:
-        port = 80  # Default HTTP port
+        port = 80
 
-    return hostname, port, path
+    return {
+        "hostname": hostname,
+        "port": port,
+        "path": path
+    }
 
 # Start of program execution
 # Parse out the command line server address and port number to listen to
 parser = OptionParser()
-parser.add_option('-p', type='int', dest='serverPort')
-parser.add_option('-a', type='string', dest='serverAddress')
+parser.add_option("-p", type="int", dest="serverPort")
+parser.add_option("-a", type="string", dest="serverAddress")
 (options, args) = parser.parse_args()
 
 port = options.serverPort
 address = options.serverAddress
 if address is None:
-    address = 'localhost'
+    address = "localhost"
 if port is None:
     port = 2100
 
@@ -65,39 +77,45 @@ if port is None:
 # Set up signal handling (ctrl-c)
 signal.signal(signal.SIGINT, ctrl_c_pressed)
 
-# TODO: Set up sockets to receive requests
-# IMPORTANT!
-# Immediately after you create your proxy's listening socket add
-# the following code (where "skt" is the name of the socket here):
-# skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Without this code the autograder may cause some tests to fail
-# spuriously.
 proxyPort = port
-proxySocket = socket(AF_INET, SOCK_DGRAM)
+proxySocket = socket(AF_INET, SOCK_STREAM)
+proxySocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 proxySocket.bind((address, port))
-proxySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+proxySocket.listen()
 print(f"Socket is listening on {address}:{port}")
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input HTTP Request:')
-clientSocket.sendto(message.encode(), (address, port))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(parse_http_request(modifiedMessage))
-clientSocket.close()
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', port))
-serverSocket.bind(('', serverPort))
-print('The server is ready to receive')
-
-
-# TODO: accept and handle connections
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    # TODO: instead of client address, proxy?
-    serverSocket.sendto(modifiedMessage.encode(), 
-    clientAddress)
-    # serverSocket.sendto(modifiedMessage.encode(), 
-    # (address, port))
+    clientConn, clientAddr = proxySocket.accept()
+    print(f"Connection received from {clientAddr}")
+
+    clientRequest = clientConn.recv(2048).decode()
+
+    try:
+        parsed_request = parse_http_request(clientRequest)
+        print("http: " + parsed_request["http_version"])
+        print("method: " + parsed_request["method"])
+        print("url: " + parsed_request["url"])
+        # print("headers: " + parsed_request["headers"])
+        url_data = parse_url(parsed_request["url"])
+
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.connect((url_data["hostname"], url_data["port"]))
+
+        server_request = f"{parsed_request["method"]} {parsed_request["http_version"]}\r\n \
+            Host: {url_data["path"]} \r\n \
+            Connection: close"
+        
+        # TODO: add headers if they exists
+        serverSocket.sendall(server_request.encode())
+
+        server_response = serverSocket.recv(url_data["port"])
+        print("Response from server received.")
+
+        clientConn.sendall(server_response)
+
+    except Exception as e:
+        print("Error:", e)
+        clientConn.sendall(b"HTTP/1.0 500 Internal Server Error\r\n\r\n")
+
+    clientConn.close()
--- Project1/PA1-A/simple_test.py
+++ Project1/PA1-A/simple_test.py
@@ -3,5 +3,6 @@ from socket import *
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(('localhost', 2100))
 sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\ntest: header\r\n\r\n')
 
 print(sock.makefile('rb').read())
