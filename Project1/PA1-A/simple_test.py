d007226b54b39e4a64bb363393b74070685cccf4
Emma Luk <emma07315@gmail.com>
2025-03-14 15:46:21 -0600

    simple_test updated

--- Project1/PA1-A/simple_test.py
+++ Project1/PA1-A/simple_test.py
@@ -2,15 +2,20 @@ from socket import *
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(('localhost', 2100))
-sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
 
-# sock.sendall(b'GET http://localhost:8765/proxy/cache/flush HTTP/1.0\r\n\r\n')
 # sock.sendall(b'GET http://localhost:8765/proxy/cache/enable HTTP/1.0\r\n\r\n')
-# sock.sendall(b'GET http://localhost:8765/ HTTP/1.0\r\n\r\n')
-# sock.sendall(b'GET http://localhost:8765/ HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://localhost:8765/proxy/cache/flush HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://localhost:8765/proxy/cache/disable HTTP/1.0\r\n\r\n')
 
-                #GET http://www.flux.utah.edu HTTP/1.0
-# sock.sendall(b'GET http://www.google.com/ HTTP/1.0\r\nConnection: keep-alive\r\n\r\n')
-# sock.sendall(b'GIBBERISH http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+sock.sendall(b'GET http://localhost:8765/proxy/blocklist/enable HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://localhost:8765/proxy/blocklist/add/google HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://google.com HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://localhost:8765/proxy/blocklist/remove/google HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://google.com HTTP/1.0\r\n\r\n')
+# sock.sendall(b'GET http://localhost:8765/proxy/blocklist/disable HTTP/1.0\r\n\r\n')
+
 
 print(sock.makefile('rb').read())
